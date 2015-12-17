169161f58e321f749f4175152126b244389e2908
Arda Xi <arda@ardaxi.com>
2015-12-17 21:19:16 +0100

    Move configuration-common contents to profiles/default

--- profiles/default.nix
+++ profiles/default.nix
@@ -1,6 +1,8 @@
 { config, pkgs, ... }:
 
 {
+  nixpkgs.config.allowUnfree = true;
+
   time.timeZone = "Europe/Amsterdam";
 
   i18n = {
@@ -9,7 +11,28 @@
     defaultLocale = "en_US.UTF-8";
   };
 
+  users.extraUsers.ardaxi = {
+    name = "ardaxi";
+    group = "ardaxi";
+    extraGroups = [
+      "users"
+      "wheel"
+      "networkmanager"
+      "audio"
+    ];
+    uid = 1000;
+    home = "/home/ardaxi";
+    createHome = true;
+    shell = "/run/current-system/sw/bin/zsh";
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC9L+dgqEWlz1HCAFvOo2WMcxXUCVZjEENrSCHEHdGKx ardaxi@hiro"
+    ];
+  };
+  users.extraGroups.ardaxi.gid = 1000;
+
   services.openssh.enable = true;
+
+  programs.zsh.enable = true;
 
   networking.networkmanager.enable = true;
 
