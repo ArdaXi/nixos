a0781542564b17e56aaba3cb2144791f216290e3
Arda Xi <arda@ardaxi.com>
2017-10-29 14:20:41 +0100

    Add CIC

--- machines/cic.nix
+++ machines/cic.nix
@@ -1,36 +1,51 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 {
   imports = [
-    <nixpkgs/nixos/modules/profiles/qemu-guest.nix>
+    <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
     ../profiles/default.nix
+    ../profiles/nas.nix
   ];
 
-  boot = {
-    initrd = {
-      availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront"];
-      supportedFilesystems = [ "ext4" ];
+  boot.initrd.availableKernelModules = [ "ahci" "ohci_pci" "ehci_pci" "pata_atiixp" "usb_storage" "usbhid" "sd_mod" ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
+  boot.supportedFilesystems = [ "zfs" ];
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.device = "/dev/disk/by-id/usb-SanDisk_Ultra_Fit_4C530001210615122264-0:0";
+
+  fileSystems."/" =
+    { device = "zones/nixos";
+      fsType = "zfs";
     };
-    kernelModules = [ "kvm-intel" ];
-    kernelParams = [ "boot.shell_on_fail" ];
-    loader.grub = {
-      enable = true;
-      version = 2;
-      device = "/dev/xvda";
+
+  fileSystems."/nix" =
+    { device = "zones/nix";
+      fsType = "zfs";
     };
+
+  fileSystems."/boot" =
+  { device = "/dev/disk/by-uuid/9634-48F6";
+    fsType = "vfat";
   };
 
-  fileSystems = {
-    "/" = {
-      device = "/dev/disk/by-label/nixos";
-      fsType = "ext4";
-    };
+  fileSystems."/media" =
+  { device = "zones/media";
+    fsType = "zfs";
   };
 
-  networking = {
-    hostId = "8425e349";
-    hostName = "cic";
-  };
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/aa8b39de-2005-4668-bdb2-507fbe3391ce"; }
+    ];
 
-  nix.maxJobs = 1;
+  nix.maxJobs = lib.mkDefault 2;
+  powerManagement.cpuFreqGovernor = "ondemand";
+
+  networking.hostId = "567f8775";
+  networking.hostName = "cic";
+
+  networking.interfaces.enp3s0.ip4 = [ { address = "192.168.178.2"; prefixLength = 24; } ];
+  networking.defaultGateway = "192.168.178.1";
+  networking.nameservers = [ "192.168.178.1" ];
 }
---
+++ profiles/nas.nix
@@ -0,0 +1,105 @@
+{ config, pkgs, ... }:
+
+{
+  networking.firewall.enable = false;
+
+  services.nfs.server = {
+    enable = true;
+    exports = ''
+      /export       *(insecure,rw,sync,no_subtree_check,crossmnt,fsid=0)
+      /export/media *(insecure,rw,sync,no_subtree_check)
+    '';
+  };
+  fileSystems."/export/media" = {
+    device = "/media";
+    options = [ "bind" ];
+  };
+
+  services.bind = {
+    enable = true;
+    cacheNetworks = [ "192.168.178.0/24" "127.0.0.0/24" "fe80::/64" "::1/128" ];
+    forwarders = [ "192.168.178.1" ];
+    zones = [{
+      name = "street.ardaxi.com";
+      master = true;
+      file = builtins.toFile "street.ardaxi.com"
+      ''
+$ORIGIN street.ardaxi.com.
+$TTL 1h
+@ IN SOA @ root (1 1h 1h 4w 1h)
+@ IN NS  ns
+@ IN A   192.168.178.2
+* IN A   192.168.178.2
+      '';
+    } {
+      name = "local.ardaxi.com";
+      master = true;
+      file = builtins.toFile "local.ardaxi.com"
+      ''
+$ORIGIN local.ardaxi.com.
+$TTL 1h
+@ IN SOA @ root (1 1h 1h 4w 1h)
+@ IN NS  ns
+@ IN A   192.168.178.2
+* IN A   192.168.178.2
+      '';
+    }];
+  };
+
+  services.nginx = {
+    enable = true;
+    virtualHosts = let
+      outsideSSL = [
+        { addr = "0.0.0.0"; port =  80; ssl = false; }
+        { addr = "0.0.0.0"; port =  81; ssl = false; }
+        { addr = "0.0.0.0"; port =  443; ssl = true; }
+	{ addr = "0.0.0.0"; port = 6443; ssl = true; }
+      ];
+    in {
+      "unifi.street.ardaxi.com" = {
+        enableACME = true;
+	forceSSL = true;
+	listen = outsideSSL;
+	locations = {
+	  "/" = {
+	    proxyPass = "https://localhost:8443";
+	    extraConfig = ''
+	      proxy_ssl_verify off;
+	      proxy_set_header Host $host;
+	      proxy_set_header X-Real-IP $remote_addr;
+	      proxy_set_header X-Forward-For $proxy_add_x_forwarded_for;
+	      proxy_http_version 1.1;
+	      proxy_set_header Upgrade $http_upgrade;
+	      proxy_set_header Connection "upgrade";
+	    '';
+	  };
+	};
+      };
+      "local.ardaxi.com" = {
+        locations = {
+	  "/" = {
+	    alias = "/var/lib/nginx/index/";
+	    index = "index.html";
+	  };
+	  "/media" = {
+	    alias = "/media";
+	    extraConfig = "autoindex on;";
+	  };
+	  "/sabnzbd/" = {
+	    proxyPass = "http://localhost:8081/";
+	  };
+	};
+      };
+    };
+  };
+
+  services.unifi.enable = true;
+  services.sabnzbd.enable = true;
+  services.hydra = {
+    enable = true;
+    hydraURL = "http://local.ardaxi.com:3000/";
+    notificationSender = "nobody@localhost";
+  };
+
+  networking.extraHosts = "127.0.0.1 ns.street.ardaxi.com";
+}
