74edda3edb4f9e0180f83c5fa6027117952d4228
Arda Xi <arda@ardaxi.com>
2019-02-16 10:12:11 +0100

    New server

--- machines/cic.nix
+++ machines/cic.nix
@@ -9,13 +9,18 @@
 
   system.stateVersion = "18.03";
 
-  boot.initrd.availableKernelModules = [ "ahci" "ohci_pci" "ehci_pci" "pata_atiixp" "usb_storage" "usbhid" "sd_mod" ];
-  boot.kernelModules = [ "kvm-amd" ];
+  boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "ehci_pci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
+  boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
   boot.supportedFilesystems = [ "zfs" ];
-  boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
-  boot.loader.grub.device = "/dev/disk/by-id/usb-SanDisk_Ultra_Fit_4C530001210615122264-0:0";
+
+  boot.loader = {
+      grub.enable = false;
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+  };
+
+  boot.tmpOnTmpfs = true;
 
   fileSystems."/" =
     { device = "zones/nixos";
@@ -28,7 +33,7 @@
     };
 
   fileSystems."/boot" =
-  { device = "/dev/disk/by-uuid/9634-48F6";
+  { device = "/dev/disk/by-uuid/B081-027E";
     fsType = "vfat";
   };
 
@@ -37,27 +42,15 @@
     fsType = "zfs";
   };
 
-  fileSystems."/tahoe" =
-  { device = "zones/e025500d-71d1-caa0-d9ec-ecf39f3acc6f";
-    fsType = "zfs";
-  };
-
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/aa8b39de-2005-4668-bdb2-507fbe3391ce"; }
+    [ { device = "/dev/disk/by-partuuid/d6c13608-1d4f-4b6b-a64b-e709ba7208d8"; }
     ];
 
-  nix.maxJobs = 1;
-  nix.daemonNiceLevel = 1;
+  nix.maxJobs = 6;
   powerManagement.cpuFreqGovernor = "ondemand";
 
   networking = {
     hostId = "567f8775";
     hostName = "cic";
-    defaultGateway = "192.168.178.1";
-    nameservers = [ "192.168.178.1" ];
-
-    bridges.br0.interfaces = [ "enp2s0" "enp3s0" ];
-
-    interfaces.br0.ip4 = [ { address = "192.168.178.2"; prefixLength = 24; } ];
   };
 }
