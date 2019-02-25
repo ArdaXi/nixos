4cbd93200684a5b73b4fdceea429e0be5cfd2590
Arda Xi <arda@ardaxi.com>
2019-02-25 10:55:49 +0100

    GPG SSH experiment

--- profiles/default.nix
+++ profiles/default.nix
@@ -43,6 +43,7 @@
     createHome = true;
     shell = "/run/current-system/sw/bin/zsh";
     openssh.authorizedKeys.keys = [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCnEDvMXrD7Hh3iQC42v2RlVs386cs3f2Z9UfTwblK8Qj65KqMoIqbNw6X0c0hW0dwYArAdQmxJBhynS+ajoNs0I+WY+SLT1DHPx/KejepzlQEYpmlSXp7bTc4B68iTy3nqTbmbxyHDHAfBeEoMDctGzuqr1wHTBM3oxaWn37OxoNQOux/RnjPWLQpv7UmUniUimrn1f6aEX93SO7GqVzqPzG3FnwcAtEaTw2tWeE349T8eG+Qf+17w/3YRwzRll9G8FKEcInnnpMSXEghkW6wZP99WeNDuOTPK9EYeb/oEOEZEvk8DXFzbDNSxBG5F2riRXL2JW1pQxYPemmiGIlnehxm1qLZ465v/LvdKS3ice6WD8s52cQaPUQiFvRxfrnfxxLxKb3mxmZJJtenIrvvDXVgmmJ+31qk0x8O8ZaaZZUI6XKw3HyfqXOgjrj1T+ppB2wNOIrKysKVbb26fviBwGGFOq9Kp/qyNcM1hMh8U7KWzUnzb77Yw1x3AzYwyf6y9i49XuxYJzrud+5yJbj9rtWby748grENM6KMVjPv8D//3MFmLKeHuohR4Ft+yBTWCbYrZU8dV5KBVfK8he2lDHG9gDCdGKjXn9jngFWBdHaXixtMMQHC0dZmycATnnR5acoN52xzj/K0sVod3jqrpT/J+SxVHbw2OpruVx6Qs+Q== openpgp:0xADB81AB8" #5C
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC9L+dgqEWlz1HCAFvOo2WMcxXUCVZjEENrSCHEHdGKx ardaxi@hiro"
     ];
   };
