710e93c3fa1982a5dc22ccbb3abb31e50a4685d1
Arda Xi <arda@ardaxi.com>
2019-04-05 19:32:59 +0200

    Add SSL to local

--- profiles/nas/default.nix
+++ profiles/nas/default.nix
@@ -102,6 +102,8 @@
       };
       "local.street.ardaxi.com" = {
         http2 = false;
+        enableACME = true;
+        addSSL = true;
         extraConfig = ''
           allow 192.168.178.0/24;
           allow 82.94.130.160/29;
