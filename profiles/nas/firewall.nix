e25e7e8a8d79825148823a5055b78342fe73ab0d
Arda Xi <arda@ardaxi.com>
2019-04-12 16:03:50 +0200

    Open transmission

--- profiles/nas/firewall.nix
+++ profiles/nas/firewall.nix
@@ -7,12 +7,14 @@
       22 53 80 443
       9001 # Weechat
       8448 # Matrix
+      51413 # Transmission
     ];
     allowedUDPPortRanges = [
       { from = 60000; to = 61000; } # mosh
     ];
     allowedUDPPorts = [
       53
+      51413 # Transmission
     ];
     extraCommands = ''
       iptables -A nixos-fw -s 192.168.178.0/24 -j nixos-fw-accept
