195ccba4fe83734fa9b121cc743c9e5bce87e5e6
Arda Xi <arda@ardaxi.com>
2020-02-28 13:46:17 +0100

    Changes to make hydra happy

--- profiles/default.nix
+++ profiles/default.nix
@@ -9,13 +9,20 @@
     package = pkgs.nixUnstable;
     binaryCachePublicKeys = [ "street.ardaxi.com-1:A1P6oGDAlLPtBbscHNTzBM6DpMHGpqLNwXUgmOtNegg=" ];
     binaryCaches = [ https://cache.nixos.org/ http://nix-cache.street.ardaxi.com/ ];
-    extraOptions = "fallback = true";
+    extraOptions = ''
+      fallback = true
+      experimental-features = nix-command
+    '';
   };
   nixpkgs = {
     config = {
       allowUnfree = true;
       allowBroken = true;
       android_sdk.accept_license = true;
+      permittedInsecurePackages = [
+         "openssl-1.0.2u"
+      ];
+# Not happy about this but it won't eval otherwise, despite not using it.
     };
     overlays = (import ../overlays);
   };
