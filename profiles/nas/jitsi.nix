31c325b40eb7d4d259e745f5151b66e257efbb54
Arda Xi <arda@ardaxi.com>
2020-04-15 19:04:53 +0200

    Revert "jitsi: aliases support"
    This reverts commit 7de99fc585cacc1ec1c2c92cc9e94cca5ffc5642.

--- profiles/nas/jitsi.nix
+++ profiles/nas/jitsi.nix
@@ -64,30 +64,10 @@ in
     };
   };
 
-  services.nginx.virtualHosts.${hostName} = let 
-    aliases = [ "meet.arien.dev" "meet.xn--arin-npa.eu" ];
-    regexPart = lib.replaceStrings ["."] ["\\."] (lib.concatStringsSep "|" aliases);
-    aliasRegex = "^https://(${regexPart})$";
-  in {
+  services.nginx.virtualHosts.${hostName} = {
     forceSSL = true;
     enableACME = true;
 
-    serverAliases = aliases;
-
-    locations."/http-bind".extraConfig = ''
-      if (''$http_origin ~ '${aliasRegex}') {
-        add_header 'Access-Control-Allow-Origin' "''$http_origin" always;
-        add_header 'Access-Control-Allow-Credentials' 'true' always;
-        add_header 'Access-Control-Allow-Methods' 'GET, POST, PUT, DELETE, OPTIONS' always;
-        add_header 'Access-Control-Allow-Headers' 'Accept,Authorization,Cache-Control,Content-Type,DNT,If-Modified-Since,Keep-Alive,Origin,User-Agent,X-Requested-With' always;
-      }
-
-      if (''$request_method = 'OPTIONS') {
-        add_header 'Access-Control-Max-Age' 1728000;
-        add_header 'Content-Type' 'text/plain charset=UTF-8';
-        add_header 'Content-Length' 0;
-        return 204;
-      }
-    '';
+    serverAliases = [ "meet.arien.dev" "meet.xn--arin-npa.eu" ];
   };
 }
