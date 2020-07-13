9369daf2bda6f023daaee2b3910531c15bb42324
Arda Xi <arda@ardaxi.com>
2020-07-13 20:28:15 +0200

    systemd-resolved

--- profiles/desktop.nix
+++ profiles/desktop.nix
@@ -144,9 +144,28 @@
 #  ];
 }
 { # Networking
-  services.avahi = {
+  services.resolved = {
     enable = true;
-    nssmdns = true;
+    dnssec = "true";
+    domains = [ "local.street.ardaxi.com" ];
+    fallbackDns = [
+      # XS4ALL
+      "194.109.6.66"
+      "194.109.9.99"
+      "194.109.104.104"
+      "2001:888:0:6::66"
+      "2001:888:0:9::99"
+
+      # Quad9
+      "9.9.9.9"
+      "149.112.112.112"
+      "2620:fe::fe"
+      "2620:fe::9"
+
+      # OpenDNS
+      "208.67.222.222"
+      "208.67.220.220"
+    ];
   };
 
   networking = {
