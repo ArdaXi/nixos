b6ab2135073202834844ffd6524f16982209a480
Arda Xi <arda@ardaxi.com>
2020-07-29 16:53:04 +0200

    Add pymfy

--- profiles/nas/homeassistant.nix
+++ profiles/nas/homeassistant.nix
@@ -1,5 +1,16 @@
 { config, lib, pkgs, ... }:
 
+let
+  pymfy = ps: ps.buildPythonPackage rec {
+    pname = "pymfy";
+    version = "0.9.0";
+    src = ps.fetchPypi {
+      inherit pname version;
+      sha256 = "sha256-6KB/YAckwRERj7jHrNT3qtoy6JABxMNek5sZhkSBWiM=";
+    };
+    propagatedBuildInputs = [ ps.requests_oauthlib ];
+  };
+in
 {
   config = lib.mkMerge [
   {
@@ -7,7 +18,7 @@
       enable = true;
       package = pkgs.home-assistant.override {
         extraPackages = ps: [
-          ps.pytado
+          (pymfy ps)
         ];
         extraComponents = [
           "met"
