7f925c1c84a2ade1f3bcb4d5dc1f9aec6d6e0b45
Arda Xi <arda@ardaxi.com>
2020-07-29 12:49:51 +0200

    hassio: http

--- profiles/nas/homeassistant.nix
+++ profiles/nas/homeassistant.nix
@@ -9,7 +9,7 @@
   }
   {
     services.nginx.virtualHosts."local.street.ardaxi.com".locations."/home/" = {
-      proxyPass = "https://127.0.0.1:${toString config.services.home-assistant.port}/";
+      proxyPass = "http://127.0.0.1:${toString config.services.home-assistant.port}/";
       extraConfig = ''
         allow 192.168.178.0/24;
         allow 82.94.130.160/29;
