2b911ed1087b86cdb21a10e8c3c9837e6f869ad6
Arda Xi <arda@ardaxi.com>
2020-07-29 16:19:45 +0200

    Hass: proxy conf

--- profiles/nas/homeassistant.nix
+++ profiles/nas/homeassistant.nix
@@ -30,6 +30,15 @@
       forceSSL = true;
       locations."/" = {
         proxyPass = "http://127.0.0.1:${toString config.services.home-assistant.port}/";
+        extraConfig = ''
+          proxy_ssl_verify off;
+          proxy_set_header Host $host;
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header X-Forward-For $proxy_add_x_forwarded_for;
+          proxy_http_version 1.1;
+          proxy_set_header Upgrade $http_upgrade;
+          proxy_set_header Connection "upgrade";
+        '';
       };
     };
   }
