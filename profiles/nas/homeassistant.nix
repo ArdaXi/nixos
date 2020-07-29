df28db668882def783f9b1a84cce52b798844943
Arda Xi <arda@ardaxi.com>
2020-07-29 16:10:13 +0200

    Hass: enable SSL

--- profiles/nas/homeassistant.nix
+++ profiles/nas/homeassistant.nix
@@ -25,14 +25,12 @@
     };
   }
   {
-    services.nginx.virtualHosts."local.street.ardaxi.com".locations."/home/" = {
-      proxyPass = "http://127.0.0.1:${toString config.services.home-assistant.port}/";
-      extraConfig = ''
-        allow 192.168.178.0/24;
-        allow 82.94.130.160/29;
-        allow 2001:984:3f27::/48;
-        deny all;
-      '';
+    services.nginx.virtualHosts."home.street.ardaxi.com" = {
+      enableACME = true;
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:${toString config.services.home-assistant.port}/";
+      };
     };
   }
   ];
