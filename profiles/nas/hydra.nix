3b3c56d023582ee46ef28b2adb32e0bbba3a784d
Arda Xi <arda@ardaxi.com>
2021-01-30 19:26:45 +0100

    Just remove the gc-check thing...

--- profiles/nas/hydra.nix
+++ profiles/nas/hydra.nix
@@ -1,30 +1,39 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 
 {
-  services = {
-    hydra = {
-      enable = true;
-      hydraURL = "https://hydra.street.ardaxi.com";
-      notificationSender = "hydra@localhost";
-      useSubstitutes = true;
-      extraConfig = "max_output_size = 4294967296";
-      package = pkgs.hydra-unstable;
-    };
-
-    nix-serve = {
-      enable = true;
-      port = 3001;
-      secretKeyFile = "/etc/nix/signing-key.sec";
+  # This is an incredibly ugly hack...
+  options = {
+    nix.extraOptions = lib.mkOption {
+      apply = builtins.replaceStrings ["gc-check-reachability"] ["#gc-check-reachability"];
     };
   };
 
-  users.users.nixbuild = {
-    name = "nixbuild";
-    shell = pkgs.bashInteractive;
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILRmMpqZs45J4+Bl0LrRpjWvM/EOz1CBtWiBWyrEK9iS"
-    ];
-  };
+  config = {
+    services = {
+      hydra = {
+        enable = true;
+        hydraURL = "https://hydra.street.ardaxi.com";
+        notificationSender = "hydra@localhost";
+        useSubstitutes = true;
+        extraConfig = "max_output_size = 4294967296";
+        package = pkgs.hydra-unstable;
+      };
 
-  nix.allowedUsers = [ "hydra" "hydra-www" "@hydra" "nix-serve" ];
+      nix-serve = {
+        enable = true;
+        port = 3001;
+        secretKeyFile = "/etc/nix/signing-key.sec";
+      };
+    };
+
+    users.users.nixbuild = {
+      name = "nixbuild";
+      shell = pkgs.bashInteractive;
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILRmMpqZs45J4+Bl0LrRpjWvM/EOz1CBtWiBWyrEK9iS"
+      ];
+    };
+
+    nix.allowedUsers = [ "hydra" "hydra-www" "@hydra" "nix-serve" ];
+  };
 }
