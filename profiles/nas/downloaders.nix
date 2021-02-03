cd6193d3cf5f73bba764195586f76000c1d4671f
Arda Xi <arda@ardaxi.com>
2021-02-03 21:15:49 +0100

    nas: remove sabnzbd

--- profiles/nas/downloaders.nix
+++ profiles/nas/downloaders.nix
@@ -3,7 +3,7 @@
   nixpkgs.config.permittedInsecurePackages = [ "p7zip-16.02" ]; # needed for sabnzbd
 
   services = {
-    sabnzbd.enable = true;
+#    sabnzbd.enable = true;
     sickrage.enable = true;
     transmission = {
       enable = true;
