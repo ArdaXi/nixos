71ee68a3a347fc65d08343a80ed8bc35346921b5
Arda Xi <arda@ardaxi.com>
2021-03-19 07:32:15 +0100

    ipfs: fix gateway address

--- profiles/nas/ipfs.nix
+++ profiles/nas/ipfs.nix
@@ -1,7 +1,10 @@
 { ... }:
 
 {
-  services.ipfs.enable = true;
+  services.ipfs = {
+    enable = true;
+    gatewayAddress = "/ip4/127.0.0.1/tcp/8181";
+  };
 
   networking.firewall.allowedTCPPorts = [ 4001 ];
 }
