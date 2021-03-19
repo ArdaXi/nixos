bed84c901f8aaa2ccb184800204f976450833a39
Arda Xi <arda@ardaxi.com>
2021-03-19 07:42:58 +0100

    nas: ipfs: listen on 0.0

--- profiles/nas/ipfs.nix
+++ profiles/nas/ipfs.nix
@@ -3,7 +3,8 @@
 {
   services.ipfs = {
     enable = true;
-    gatewayAddress = "/ip4/127.0.0.1/tcp/8181";
+    gatewayAddress = "/ip4/0.0.0.0/tcp/8181";
+    apiAddress = "/ip4/0.0.0.0/tcp/5001";
   };
 
   networking.firewall.allowedTCPPorts = [ 4001 ];
