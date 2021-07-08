1979bc0996a9293a4f8092f8e99b43fe3937c58f
Arda Xi <arda@ardaxi.com>
2021-07-08 10:53:52 +0200

    nas: open ptp

--- profiles/nas/firewall.nix
+++ profiles/nas/firewall.nix
@@ -27,6 +27,7 @@
       iptables -A nixos-fw -s 192.168.178.0/24 -j nixos-fw-accept
       iptables -A nixos-fw -s 192.168.179.22/32 -j nixos-fw-accept
       iptables -A nixos-fw -s 192.168.179.0/24 -p tcp --dport 1883 -j nixos-fw-accept
+      iptables -A nixos-fw -s 10.145.22.0/24 -j nixos-fw-accept
     '';
   };
 
