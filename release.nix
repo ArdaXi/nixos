720f7632158f6e672bbe49040d617a432f59bc14
Arda Xi <arda@ardaxi.com>
2021-08-27 07:01:09 +0200

    Add tests to release.nix

--- release.nix
+++ release.nix
@@ -18,6 +18,11 @@ let
     config = { allowUnfree = true; };
   };
 
+  allTests = (import <nixpkgs/nixos/tests/all-tests.nix> {
+    inherit system pkgs;
+    callTest = t: hydraJob t.test;
+  });
+
   config = hostName: (hydraJob
     (import <nixpkgs/nixos/lib/eval-config.nix> {
       inherit system;
@@ -39,5 +44,12 @@ in rec {
   machines = recImport {
     dir = ./hosts;
     _import = config;
+  };
+
+  tests = {
+    inherit (allTests) acme bat firefox grafana hydra login nginx nzbget
+      openssh prometheus signal-desktop sudo sway wireguard zfs;
+
+    inherit (allTests.postgresql) postgresql_12;
   };
 }
