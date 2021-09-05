300251b94f2eee8184e4c5b2639c198af1ae1ae8
Arda Xi <arda@ardaxi.com>
2021-09-05 08:18:53 +0200

    PAM hacks

--- profiles/core/security.nix
+++ profiles/core/security.nix
@@ -1,9 +1,40 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 {
-  environment.systemPackages = [ pkgs.pam_u2f ];
+  options = {
+    security.pam.services = lib.mkOption {
+      type = lib.types.attrsOf (lib.types.submodule ({config, ...}: {
+        options.u2fPin = lib.mkOption {
+          default = false;
+          type = lib.types.bool;
+        };
+        options.text = lib.mkOption {
+          apply = v: if config.u2fPin then builtins.replaceStrings [ "pam_u2f.so" ] [ "pam_u2f.so pinverification=1" ] v else v;
+          };
+      }));
+    };
+  };
+  config = {
+    environment.systemPackages = [ pkgs.pam_u2f ];
 
-  security.doas.enable = true;
+    security.doas = {
+      enable = true;
+      extraRules = [ 
+        {
+          groups = [ "wheel" ];
+          persist = true;
+        }
+      ];
+    };
 
-  security.pam.services.doas.u2fAuth = true;
+    security.pam.u2f.cue = true;
+
+    security.pam.services = {
+      sudo.u2fAuth = true;
+      doas = {
+        u2fAuth = true;
+#        u2fPin = true;
+      };
+    };
+  };
 }
