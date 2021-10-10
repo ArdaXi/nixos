16f829c94898c84fd79f3148fcb930e19d799f06
Arda Xi <arda@ardaxi.com>
2021-10-10 16:32:12 +0200

    nas: persistent (private) iscsi configuration

--- profiles/nas/iscsi.nix
+++ profiles/nas/iscsi.nix
@@ -4,4 +4,17 @@
   boot.kernelModules = [ "configfs" "target_core_mod" "iscsi_target_mod" ];
 
   environment.systemPackages = [ pkgs.targetcli ];
+
+  systemd.services.iscsi-target = {
+    enable = true;
+    after = [ "network.target" "local-fs.target" ];
+    requires = [ "sys-kernel-config.mount" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.python3.pkgs.rtslib}/bin/targetctl restore";
+      ExecStop = "${pkgs.python3.pkgs.rtslib}/bin/targetctl clear";
+      RemainAfterExit = "yes";
+    };
+  };
 }
