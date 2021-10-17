e0aa2652a84641da633039e634233c871b48700f
Arda Xi <arda@ardaxi.com>
2021-10-17 15:00:33 +0200

    hiro: allow discards

--- hosts/hiro.nix
+++ hosts/hiro.nix
@@ -60,7 +60,10 @@
   boot = {
     initrd = {
       availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" "thunderbolt" ];
-      luks.devices.cypher.device = "/dev/disk/by-uuid/9c91fb6e-5dc5-4492-982e-adf996479106";
+      luks.devices.cypher = {
+        device = "/dev/disk/by-uuid/9c91fb6e-5dc5-4492-982e-adf996479106";
+        allowDiscards = true;
+      };
     };
 
     kernel.sysctl."net.ipv6.route.max_size" = "2147483647";
