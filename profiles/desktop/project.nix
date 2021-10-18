414f89e01ee331a00b84c5ef571989024cd23b19
Arda Xi <arda@ardaxi.com>
2021-10-18 12:18:28 +0200

    hiro: add libvirtd

--- profiles/desktop/project.nix
+++ profiles/desktop/project.nix
@@ -3,7 +3,17 @@
 {
   environment.systemPackages = [
     pkgs.teams
+    pkgs.virt-manager
   ];
 
   virtualisation.virtualbox.host.enable = true;
+
+  virtualisation.libvirtd = {
+    enable = true;
+    qemuOvmfPackage = pkgs.OVMFFull;
+    qemuSwtpm = true;
+    qemuRunAsRoot = false;
+  };
+
+  users.users.ardaxi.extraGroups = [ "libvirtd" ];
 }
