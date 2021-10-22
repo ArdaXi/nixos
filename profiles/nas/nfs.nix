a3a5d1277d0135ed0e26bedb912a838b9e9ac562
Arda Xi <arda@ardaxi.com>
2021-10-22 18:00:23 +0200

    nas: update nfs exports

--- profiles/nas/nfs.nix
+++ profiles/nas/nfs.nix
@@ -4,13 +4,15 @@
   services.nfs.server = {
     enable = true;
     exports = ''
-      /export       *(insecure,rw,sync,no_subtree_check,crossmnt,fsid=0)
-      /export/media *(insecure,rw,sync,no_subtree_check)
+      /export             *(insecure,rw,sync,no_subtree_check,crossmnt,fsid=0)
+      /export/media       *(insecure,rw,sync,no_subtree_check,crossmnt,fsid=1)
+      /export/media/tv    *(insecure,rw,sync,no_subtree_check,nohide,fsid=2)
+      /export/media/films *(insecure,rw,sync,no_subtree_check,nohide,fsid=3)
     '';
   };
 
   fileSystems."/export/media" = {
     device = "/media";
-    options = ["bind"];
+    options = ["rbind"];
   };
 }
