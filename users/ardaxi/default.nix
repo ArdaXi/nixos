5621cd65b305364a6e7c4e95bd8a6af1b8104ff6
Arda Xi <arda@ardaxi.com>
2021-10-23 11:32:02 +0200

    users: add SK ssh key

--- users/ardaxi/default.nix
+++ users/ardaxi/default.nix
@@ -8,6 +8,7 @@
     createHome = true;
     shell = "/run/current-system/sw/bin/zsh";
     openssh.authorizedKeys.keys = [
+      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIGF+jT+vAfFpVuylZbfp2ZInVHo4J31xlG1ytGoqsgT/AAAABHNzaDo= ardaxi@hiro"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC9L+dgqEWlz1HCAFvOo2WMcxXUCVZjEENrSCHEHdGKx ardaxi@hiro"
     ];
     isNormalUser = true;
