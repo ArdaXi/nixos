f3c029b0db4d7a1e08be57f37068e320c418ce11
Arda Xi <soft.water0852@ardaxi.com>
2022-05-26 17:40:32 +0200

    Remove hydra patch

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -50,11 +50,12 @@ rec {
   mudlet = final.libsForQt5.callPackage ./mudlet.nix { lua = final.lua5_1; };
 
   nix-hydra = final.nixVersions.nix_2_6 or final.nix;
-  hydra-unstable = (prev.hydra-unstable.overrideAttrs (oldAttrs: {
-    patches = oldAttrs.patches or [] ++ [
-      ./hydra-no-restrict.patch
-    ];
-  })).override { nix = nix-hydra; };
+#  hydra-unstable = (prev.hydra-unstable.overrideAttrs (oldAttrs: {
+#    patches = oldAttrs.patches or [] ++ [
+#      ./hydra-no-restrict.patch
+#    ];
+#  })).override { nix = nix-hydra; };
+  hydra-unstable = prev.hydra-unstable.override { nix = nix-hydra; };
 
 #  calibre = final.libsForQt5.callPackage ./calibre.nix {};
 #  anki-bin = final.python3Packages.callPackage ./anki/anki.nix {
