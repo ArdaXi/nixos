3fc14727e78d3aa8668d6e6a1402c7862dd790c1
Arda Xi <soft.water0852@ardaxi.com>
2023-01-29 14:23:35 +0100

    Switch to pipewire

--- profiles/desktop/multimedia.nix
+++ profiles/desktop/multimedia.nix
@@ -1,10 +1,29 @@
 { pkgs, ... }:
 
 {
-  hardware.pulseaudio = {
-    package = pkgs.pulseaudioFull;
+#  hardware.pulseaudio = {
+#    package = pkgs.pulseaudioFull;
+#    enable = true;
+#    support32Bit = true;
+#  };
+
+  security.rtkit.enable = true;
+  services.pipewire = {
     enable = true;
-    support32Bit = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  environment.etc = {
+    "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
+      bluez_monitor.properties = {
+        ["bluez5.enable-sbc-xq"] = true,
+        ["bluez5.enable-msbc"] = true,
+        ["bluez5.enable-hw-volume"] = true,
+        ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+      }
+    '';
   };
 
   environment.systemPackages = with pkgs; [ mpv alsa-utils pavucontrol ];
