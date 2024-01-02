32088cdfab11f06b395ad83584280f823050e381
Arda Xi <soft.water0852@ardaxi.com>
2024-01-02 12:03:55 +0100

    Fix languagetool

--- profiles/nas/languagetool.nix
+++ profiles/nas/languagetool.nix
@@ -1,46 +1,14 @@
 { config, pkgs, ... }:
 
 {
-  systemd.services.languagetool = {
-    description = "Languagetool";
-    after = [ "network.target" ];
-    wantedBy = [ "multi-user.target" ];
-    environment = {
-      "JAVA_TOOL_OPTIONS" = "-Xms2G -Xmx2G";
-    };
-    serviceConfig = {
-      Type = "simple";
-      ExecStart = ''
-        ${pkgs.languagetool}/bin/languagetool-http-server \
-        --port 9111 \
-        --public \
-        --allow-origin \
-        --languageModel /var/lib/languagetool/ngrams \
-        --word2vecModel /var/lib/languagetool/word2vec
-      '';
-      DynamicUser = true;
-      LockPersonality = true;
-      NoNewPrivileges = true;
-      #MemoryDenyWriteExecute = true;
-      ProtectSystem = "strict";
-      ProtectClock = true;
-      ProtectKernelTunables = true;
-      ProtectKernelModules = true;
-      ProtectKernelLogs = true;
-      ProtectControlGroups = true;
-      RestrictAddressFamilies = [ "AF_NETLINK" "AF_INET6" "AF_INET" ];
-      RestrictNamespaces = true;
-      RestrictRealtime = true;
-      RestrictSUIDSGID = true;
-      RemoveIPC = true;
-      ProtectHome = true;
-      ProtectHostname = true;
-      ProtectProc = "invisible";
-      PrivateMounts = true;
-      PrivateTmp = true;
-      PrivateDevices = true;
-      PrivateUsers = true;
-      ProcSubset = "pid";
+  services.languagetool = {
+    enable = true;
+    port = 9111;
+    public = true;
+    allowOrigin = "";
+    settings = {
+      languageModel = "/var/lib/languagetool/ngrams";
+     #  --word2vecModel /var/lib/languagetool/word2vec
     };
   };
 }
