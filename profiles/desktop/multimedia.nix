1d4c815b41a23005925c9db2876856f15169b3e2
Arda Xi <soft.water0852@ardaxi.com>
2024-03-16 20:45:20 +0100

    Fix wireplumber failure

--- profiles/desktop/multimedia.nix
+++ profiles/desktop/multimedia.nix
@@ -13,8 +13,19 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    wireplumber.configPackages = [
+      (pkgs.writeTextDir "share/wireplumber/bluetooth.lua.d/51-bluez-config.lua" ''
+        bluez_monitor.properties = {
+          ["bluez5.enable-sbc-xq"] = true,
+          ["bluez5.enable-msbc"] = true,
+          ["bluez5.enable-hw-volume"] = true,
+          ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+        }
+      '')
+    ];
   };
 
+/*
   environment.etc = {
     "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
       bluez_monitor.properties = {
@@ -25,6 +36,7 @@
       }
     '';
   };
+*/
 
   environment.systemPackages = with pkgs; [ mpv alsa-utils pavucontrol ];
 }
